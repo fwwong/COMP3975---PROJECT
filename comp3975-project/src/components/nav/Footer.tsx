6467eca252fcf1fc2f5d36381d63a4442750910c
Fiona Wong <fwwong01@gmail.com>
2024-04-10 21:31:36 -0700

    fixed the footer

--- comp3975-project/src/components/nav/Footer.tsx
+++ comp3975-project/src/components/nav/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <div className="bg-gray-800 p-5 text-center text-white fixed bottom-0 w-full">
+      <div className="bg-gray-800 p-5 text-center text-white bottom-0 w-full">
         <p>&copy; 2024 - COMP3975 - Group D - Market Mingle</p>
       </div>
     );
